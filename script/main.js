d0361dd37e19c493656ddb2065b7f5443fc85cb8
sduu <qjqmfrja00@gmail.com>
2022-05-08 17:46:38 +0900

    feat: btn-shop mouse hover 인터렉션 추가

--- script/main.js
+++ script/main.js
@@ -245,6 +245,22 @@
     }
   }
 
+  /* btn-shop hover interaction */
+  function enterBtnShop(e) {
+    gsap
+      .timeline()
+      .fromTo(e.target, {rotate: 0, scale: 1}, {duration: 0.2, rotate: -30, scale: 1.1})
+      .to(e.target, {duration: 0.5, ease: Back.easeOut.config(1.7), rotate: 360});
+  }
+  function leaveBtnShop(e) {
+    gsap
+      .timeline()
+      .fromTo(e.target, {rotate: 0, scale: 1.1}, {duration: 0.2, rotate: 30, scale: 1})
+      .to(e.target, {duration: 0.5, ease: Back.easeOut.config(1.7), rotate: -360});
+  }
+
   /* init event */
   window.addEventListener('load', initCanvas);
+  document.querySelector('.footer .btn-shop i').addEventListener('mouseenter', enterBtnShop);
+  document.querySelector('.footer .btn-shop i').addEventListener('mouseleave', leaveBtnShop);
 })();
